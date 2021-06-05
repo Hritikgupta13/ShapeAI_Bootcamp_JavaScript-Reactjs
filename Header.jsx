ba1bf91041739ed5d596c31ebf13181c35feac94
Hritikgupta13 <85361394+Hritikgupta13@users.noreply.github.com>
2021-06-06 01:54:10 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Hritik's Company</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>AboutUs</h1>
+      <p>
+        Our company is a global software manufacturer and provides projects in
+        shortest time.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
